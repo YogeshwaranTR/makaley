d21397d23252f39e597fbefa765ce15a4611217b
YOGESHWARAN TR <109059451+YogeshwaranTR@users.noreply.github.com>
2022-10-17 23:28:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h2>Test</h2>
-    <h2>User1</h2>
+    <h2>merge</h2>
+    <h2>USER1.0</h2
     <h3>user2</h3>
     </div>
   );
