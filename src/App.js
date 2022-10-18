47f255d696a53c8991f278509cfa0be3945a12cd
Babysudha <babysudha.petchimuthu@crud-operations.com>
2022-10-18 12:45:28 +0530

    line create

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ function App() {
   return (
     <div className="App">
     <h2>Test</h2>
+    <h3>sudha</h3>
+    <p>vedhika</p>
     <p>sudha baby</p>
-      <p>welcome to merge confilt</p>
+    <p>welcome to merge confilt</p>
     <h2>merge</h2>
     <h2>USER1.0</h2>
     <h3>user2</h3>
